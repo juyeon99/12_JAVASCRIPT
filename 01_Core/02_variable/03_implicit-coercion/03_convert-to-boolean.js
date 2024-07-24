3ed1207d0fcd779a3659957062671da92a3b00cd
juyeon99 <beive999@naver.com>
2024-07-24 12:15:13 +0900

    implicitly converting data types into boolean

--- 01_Core/02_variable/03_implicit-coercion/03_convert-to-boolean.js
+++ 01_Core/02_variable/03_implicit-coercion/03_convert-to-boolean.js
@@ -1,25 +1,18 @@
-// 1. Boolean 생성자 함수를 new 연산자 없이 호출하는 방법
-console.log(Boolean('Javascript'));         // true
-console.log(new Boolean('JavaScript'));     // [Boolean: true]
-console.log(Boolean(''));                   // false
-console.log(Boolean(1));                    // true
-console.log(Boolean(0));                    // false
-console.log(Boolean(NaN));                  // false
-console.log(Boolean(Infinity));             // true
-console.log(Boolean(null));                 // false
-console.log(Boolean(undefined));            // false
-console.log(Boolean({}));                   // true
-console.log(Boolean([]));                   // true
+// 암묵적 타입 형변환
+/*
+    JS 엔진은 Boolean 타입이 아닌 값들을 
+    Truthy 값 (참으로 평가되는 값) / Falsy 값 (거짓으로 평가되는 값)으로 구분한다.
+*/
+if('JavaScript') {  // truthy 값이기 때문에 true로 형변환 됨
+    console.log("if('JavaScript')");
+}
+if(true)        console.log("if(true)");
+if(false)       console.log("if(false)");
+if(undefined)   console.log("if(undefined)");
+if(null)        console.log("if(null)");
+if(0)           console.log("if(0)");
+if(NaN)         console.log("if(NaN)");
+if('')          console.log("if('')");
 
-// 2. ! 부정 논리 연산자를 두 번 사용하는 방법
-console.log("==== 부정 논리 연산자 ====");
-console.log(!!'JavaScript');                // true
-console.log(!!'');                          // false
-console.log(!!1);                           // true
-console.log(!!0);                           // false
-console.log(!!NaN);                         // false
-console.log(!!Infinity);                    // true
-console.log(!!null);                        // false
-console.log(!!undefined);                   // false
-console.log(!!{});                          // true
-console.log(!![]);                          // true
+// false, undefined, null, 0, NaN, ''(빈 문자열) => falsy 값
+// 이외 모든 값은 truthy 값이다.
