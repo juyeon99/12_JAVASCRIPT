5f6ce35fe92f2c4fe76df6c0fbe8d151d3f28448
juyeon99 <beive999@naver.com>
2024-07-24 11:09:40 +0900

    data type - symbol & object

---
+++ 01_Core/02_variable/01_data-type/05_symbol-and-object.js
@@ -0,0 +1,11 @@
+// 이름이 충돌할 위험이 없는 유일한 값인 symbol을 property key로 사용
+
+// symbol
+obj[key] = 'value';
+// console.log(obj[key]);
+
+/* 
+    객체 타입
+    JS의 데이터 타입은 원시타입(number, string, boolean, undefined, null, symbol)
+    이 이외의 모든 값은 객체 타입(객체, 함수, 배열...)
+*/
