3a4a1466676a5ae602691c569cc11872140639b1
juyeon99 <beive999@naver.com>
2024-07-30 09:55:02 +0900

    Edit comments

--- 01_Core/05_function/01_function-definition/01_function-declaration.js
+++ 01_Core/05_function/01_function-definition/01_function-declaration.js
@@ -14,7 +14,7 @@ function hello(name) {
 };
 
 JS 엔진은 생성된 함수를 호출하기 위해 함수 이름과 동일한 식별자를 암묵적으로 생성하고 거기에 함수 객체 할당
-var hello = function hello(name) {
+var hello = function(name) {
     return `${name}님 안녕하세요.`;
 };
 
